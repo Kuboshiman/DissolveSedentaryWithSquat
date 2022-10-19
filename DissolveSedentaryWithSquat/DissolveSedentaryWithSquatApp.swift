e46f09d215bf8d4c521d8dd77a99fafa437f7717
kubo <kubo.shogo@arakawa-lab.com>
2022-10-19 18:25:01 +0900

    Initial Commit

---
+++ DissolveSedentaryWithSquat/DissolveSedentaryWithSquatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DissolveSedentaryWithSquatApp.swift
+//  DissolveSedentaryWithSquat
+//
+//  Created by arakawaLab on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct DissolveSedentaryWithSquatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
